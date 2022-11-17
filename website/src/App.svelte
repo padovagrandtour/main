26b6cf5d1dc0377787de942bd05ff55bb4852a31
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-11-17 10:06:32 +0100

    Added logo, basic layout

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,22 +1,44 @@
 <script>
-  import Counter from './lib/Counter.svelte'
+  import Logo from './assets/logo.webp'
 </script>
 
-<main>
   <aside>
-
+    <p>ciao</p>
   </aside>
+  <main>  
+    <div class="content">
+      <img class="logo" src={Logo}>
+    </div>
+  </main>
 
-  <h1>Padova Grand Tour</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-
-
-</main>
 
 <style>
+:global(html, body, #app){
+  margin:0;
+  padding:0;
+  font-family: Arial, Helvetica, sans-serif;
+}
 
+:global(#app){
+  display: flex;
+  flex-direction: row;
+}
+
+
+aside {
+  background-color: brown;
+  width: 300px;
+  flex: 0 0 auto;
+  height: 100vh;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+main {flex: 1 1 auto;}
+main > .content {max-width: 500px; margin: 0 auto; padding: 30px;}
+
+.logo {
+  width: 60%;
+  margin: 30px auto;
+  display: block;
+}
 </style>
