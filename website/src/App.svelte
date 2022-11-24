86007ef884cebd97639ba6dfb5531aab625d1f04
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-11-24 14:44:49 +0100

    Map working

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -1,99 +1,13 @@
 <script>
   import Logo from './assets/logo.webp'
+  import Map from './components/Map.svelte'
 
+  import { tours, selectedTour, tourStepsPromise } from './utils.js'
 
-  const sparqlPrefixes = `
-  PREFIX owl: <http://www.w3.org/2002/07/owl#> 
-  PREFIX pgt: <https://padovagrandtour.github.io/entitites#> 
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-  PREFIX xml: <http://www.w3.org/XML/1998/namespace> 
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
-  PREFIX sdo: <https://schema.org/>
-  PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-
-  `
-
-
-  /** Utility function to make simpler to query with comunica. */
-  const query = (queryStr, labels) => new Promise( (resolve, reject) => {
-    const results = []
-    new window.Comunica.QueryEngine().queryBindings(sparqlPrefixes + queryStr, {
-      sources: [{type:'sparql', value:'http://localhost:7210/repositories/pgt'}],
-    }).then(function (bindingsStream) {
-      bindingsStream.on('data', function (data) {
-        
-        results.push( labels.map( label => data.get(label) ? data.get(label).value : null))
-      });
-      bindingsStream.on('end', function () {
-        resolve(results)
-      })
-      bindingsStream.on('error', (error) => {
-          reject(error)
-      });
-    }).catch( (...e) => reject(...e));
-  })
-
-  /* Repeatedly try to query the database until you get the tour list */
-  let tours = null
-  const refreshTours = async() => {
-    try {
-      // Query tours
-      tours = await query(`
-          SELECT * {
-            ?tourID a pgt:Tour .
-            ?tourID sdo:name ?tourName .
-          } LIMIT 1000
-        `, ['tourID', 'tourName'])
-    }catch(e){
-      console.log("Failure connection", e)
-      await new Promise(resolve => setTimeout(resolve, 1000))
-      refreshTours()
-    }
-  }
-
-  refreshTours()
-
-
-
-  let selectedTour = "NOTOUR"
-
-  let tourStepsPromise = Promise.resolve([])
-
-  $:{
-
-    if(selectedTour !== 'NOTOUR'){
-      tourStepsPromise = query(`
-        SELECT ?stepIndexNumber ?siteName ?siteLat ?siteLong WHERE {
-          ?tour a pgt:Tour.
-          ?tour sdo:name "Tour 1".
-          ?tour pgt:steps ?stepNode .
-          ?stepNode ?stepIndex ?site .
-
-          BIND (STRLEN("http://www.w3.org/1999/02/22-rdf-syntax-ns#_") AS ?prefixLength)
-          BIND (xsd:integer(SUBSTR(xsd:string(?stepIndex),?prefixLength + 1)) AS ?stepIndexNumber).
-
-          OPTIONAL{ ?site sdo:name ?siteName. }
-          OPTIONAL{ ?site geo:lat  ?siteLat.  }
-          OPTIONAL{ ?site geo:long ?siteLong. }
-
-
-        } ORDER BY ?stepIndexNumber`, ['stepIndexNumber', 'siteName', 'siteLat', 'siteLong'])
-        
-    }
-  }
-
-
-  let l = ['s', 'p', 'o']
-  let r = query(`
-      SELECT * {
-        ?s ?p ?o
-      } LIMIT 100
-    `, l)
 
 </script>
 
-  {#if !tours}
+  {#if !$tours}
   <div class="loading">
     <span class="loader"></span>
     <p>connecting....</p> 
@@ -102,20 +16,20 @@
   {:else}
   <aside>
     <div class="toolbar">
-      <select bind:value={selectedTour} >
+      <select bind:value={$selectedTour} >
         <option value="NOTOUR">-- Choose a tour --</option>		
-        {#each tours as tour}
+        {#each $tours as tour}
           <option value={tour[0]}>{tour[1]}</option>
         {/each}
       </select>
     </div>
     
-    <div>
-      {#await tourStepsPromise}
+    <div class="siteList">
+      {#await $tourStepsPromise}
         <p>Loading sites...</p>
       {:then tourSteps} 
-        {#each tourSteps as step}
-          <p>{step[1]}</p>
+        {#each tourSteps as step, idx}
+          <p><b>{idx + 1}.</b> {step[1]}</p>
         {/each}
       {/await}
     </div>
@@ -123,10 +37,12 @@
   </aside>
   <main>  
     <div class="content">
-      {#if selectedTour === 'NOTOUR'}
+      {#if $selectedTour === 'NOTOUR'}
       <img class="logo" alt="Padova Grand Tour logo" src={Logo}>
       {:else}
-        <p>{selectedTour}</p>
+        <p>{$selectedTour}</p>
+
+        <Map></Map>
       {/if}
     </div>
   </main>
@@ -142,6 +58,7 @@
   margin:0;
   padding:0;
   font-family: Arial, Helvetica, sans-serif;
+  height: 100%;
 }
 
 :global(#app){
@@ -154,10 +71,29 @@ aside {
   background-color: #9b0014;
   width: 300px;
   flex: 0 0 auto;
-  height: 100vh;
+  height: 100%;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   padding: 20px 2vw;
   box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  overflow-y: hidden;
+}
+
+
+aside .siteList {
+  background-color: #fff;
+  overflow-y: auto;
+  margin: 20px 0;
+  box-sizing: border-box;
+}
+
+aside .siteList > p {
+  padding: 8px;
+  margin: 0;
+}
+aside .siteList > p:nth-child(even)  {
+  background-color: #ddd;
 }
 
 main {flex: 1 1 auto;}
