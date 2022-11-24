1f6622504d5331f83fb7fc376b194f22da179084
Luca Fabbian <luca.fabbian.1999@gmail.com>
2022-11-24 16:03:58 +0100

    entity website boilerplate test

---
+++ website-entities/src/components/Map.svelte
@@ -0,0 +1,62 @@
+<script>
+    import * as L from 'leaflet';
+    import 'leaflet/dist/leaflet.css';
+    import { tourStepsPromise } from '../utils.js'
+    let map;
+
+    let markerLocations = [];
+
+    tourStepsPromise.subscribe( async ($tourStepsPromise) =>{
+      markerLocations = (await $tourStepsPromise).filter(marker => marker[2] !== null && marker[3] !== null)
+    })
+
+
+    let lastMarkers = null
+    $: {
+      if(map){
+        if(lastMarkers !== null){
+          lastMarkers.forEach( marker => marker.remove())
+        }
+        lastMarkers = markerLocations
+          .map( marker => L.marker([marker[2], marker[3]]))
+        lastMarkers.forEach( (marker, index) => {
+          marker.addTo(map)
+          marker.bindPopup(`<b>${markerLocations[index][0]}</b>. ${markerLocations[index][1]}`)
+
+        })
+      }
+    }
+    
+  
+    function createMap(container) {
+      let m = L.map(container).setView([45.405027777777775, 11.8839], 13);
+      L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          maxZoom: 14,
+        }
+      ).addTo(m);
+  
+      return m;
+    }
+  
+    function mapAction(container) {
+      map = createMap(container);
+      return {
+        destroy: () => {
+          map.remove();
+        },
+      };
+    }
+
+    function resizeMap() {
+      if(map) { map.invalidateSize(); }
+    }
+
+  </script>
+  
+  <svelte:window on:resize={resizeMap} />
+
+
+  <div style="height:400px;width:100%" use:mapAction />
+  
---
+++ website-entities/src/utils.js
@@ -0,0 +1,99 @@
+import { writable, get } from "svelte/store";
+
+
+/**
+ * QUERY ENGINE
+ * 
+ */
+
+
+const sparqlPrefixes = `
+PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+PREFIX pgt: <https://padovagrandtour.github.io/entitites#> 
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
+PREFIX xml: <http://www.w3.org/XML/1998/namespace> 
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+PREFIX sdo: <https://schema.org/>
+PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
+
+`
+
+
+/** Utility function to make simpler to query with comunica. */
+const query = (queryStr, labels) => new Promise( (resolve, reject) => {
+const results = []
+new window.Comunica.QueryEngine().queryBindings(sparqlPrefixes + queryStr, {
+    sources: [{type:'sparql', value:'http://localhost:7210/repositories/pgt'}],
+}).then(function (bindingsStream) {
+    bindingsStream.on('data', function (data) {
+    
+    results.push( labels.map( label => data.get(label) ? data.get(label).value : null))
+    });
+    bindingsStream.on('end', function () {
+    resolve(results)
+    })
+    bindingsStream.on('error', (error) => {
+        reject(error)
+    });
+}).catch( (...e) => reject(...e));
+})
+
+
+
+
+/**
+ * APP STORE
+ * 
+ */
+
+
+
+  /* Repeatedly try to query the database until you get the tour list */
+  export const tours = writable(null)
+  const refreshTours = async() => {
+    try {
+      // Query tours
+      tours.set(await query(`
+          SELECT * {
+            ?tourID a pgt:Tour .
+            ?tourID sdo:name ?tourName .
+          } LIMIT 1000
+        `, ['tourID', 'tourName']))
+    }catch(e){
+      console.log("Failure connection", e)
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      refreshTours()
+    }
+  }
+
+  refreshTours()
+
+
+
+  export const selectedTour = writable("NOTOUR")
+
+  export const tourStepsPromise = writable(Promise.resolve([]))
+
+  selectedTour.subscribe($selectedTour => {
+    if($selectedTour !== 'NOTOUR'){
+      tourStepsPromise.set(query(`
+        SELECT DISTINCT ?stepIndexNumber ?siteName ?siteLat ?siteLong WHERE {
+          <${$selectedTour}> pgt:steps ?stepNode .
+          ?stepNode ?stepIndex ?site .
+
+          FILTER (strstarts(str(?stepIndex), 'http://www.w3.org/1999/02/22-rdf-syntax-ns#_'))
+
+          BIND (STRLEN("http://www.w3.org/1999/02/22-rdf-syntax-ns#_") AS ?prefixLength)
+          BIND (xsd:integer(SUBSTR(xsd:string(?stepIndex),?prefixLength + 1)) AS ?stepIndexNumber).
+
+          OPTIONAL{ ?site sdo:name ?siteName. }
+          OPTIONAL{ ?site geo:lat  ?siteLat.  }
+          OPTIONAL{ ?site geo:long ?siteLong. }
+
+
+        } ORDER BY ?stepIndexNumber`, ['stepIndexNumber', 'siteName', 'siteLat', 'siteLong']))
+        get(tourStepsPromise).then( r => console.log(r))
+    }
+
+  })
